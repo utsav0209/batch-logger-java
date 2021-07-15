ba03197e4688d68098ec7dc01ed49734942d1e0f
utsav0209 <utsav99varia@gmail.com>
2021-07-15 11:12:25 +0530

    INIT: Initialized Micronaut project

---
+++ src/test/java/logger/BatchLoggerJavaTest.java
@@ -0,0 +1,20 @@
+package logger;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import javax.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@MicronautTest
+class BatchLoggerJavaTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
